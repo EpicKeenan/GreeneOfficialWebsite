de8a324fbefe7838951f878a31e65f5cc4d0281d
EpicKeenan <Uberkeenan@gmail.com>
2017-03-14 07:52:41 -0500

    Update trappist-1-jquery.js

--- trappist-1-jquery.js
+++ trappist-1-jquery.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     //Basic Selectors
   
     //Animators
-    $('p').hide().delay(400).slideDown(800);
+    $('*').hide().delay(400).slideDown(800);
     //Index Filters
   
     //Relationship Filters
